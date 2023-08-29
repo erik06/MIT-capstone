5042610c3f660ac411a050b5c28ba4d0381d3172
Erik Slagter <erikslagter@gmail.com>
2023-08-29 14:44:48 -0500

    new urls for images

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: "https",
+        hostname: "strapi-75qo.onrender.com",
+        pathname: "/uploads/**",
+      },
+      {
         protocol: "http",
         hostname: "127.0.0.1",
         port: "1337",
