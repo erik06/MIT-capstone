c90a63a0e4baef5007499b7ea0cd7279c0575da7
Erik Slagter <erikslagter@gmail.com>
2023-09-04 21:12:06 -0500

    adding cloudinary

--- backend/config/env/production/plugins.js
+++ backend/config/env/production/plugins.js
@@ -1,4 +1,20 @@
-module.exports = {
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
   graphql: {
     config: {
       endpoint: "/graphql",
@@ -12,4 +28,4 @@ module.exports = {
       },
     },
   },
-};
+});
